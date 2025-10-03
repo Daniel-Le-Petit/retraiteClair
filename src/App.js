deb769ec131c6708d0c3f1e64cab788eb32a5796
Daniel Le Petit <dlepetit@hotmail.fr>
2025-10-03 11:57:51 +0200

    Feat: solution React router pour static site

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import PageAccueil from './components/PageAccueil';
 import CalculateurAvance from './components/CalculateurAvance';
